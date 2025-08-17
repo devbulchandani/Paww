2bef62a3d74cc3a65e732d78bb278ff1c1b7cf08
Dev Bulchandani <devbulchandani8@gmail.com>
2025-08-17 18:49:54 +0530

    Added cors url

--- paww/src/main/java/com/example/paww/config/CorsConfig.java
+++ paww/src/main/java/com/example/paww/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://paww-eight.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
